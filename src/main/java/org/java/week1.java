da575017b6d588b49d9cc34415f3c918e66a877a
joomart <joomart@recruitics.com>
2025-03-30 00:06:16 +0600

    add a loop and changed the main class

---
+++ src/main/java/org/java/week1.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class week1 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int number = Math.abs(rand.nextInt()) % 100;
+        System.out.println(number);
+
+        Scanner sc = new Scanner(System.in);
+//        int guess = sc.nextInt();
+
+        int guess = -1;
+        while (guess != number){
+            guess = sc.nextInt();
+
+            if (number == guess){
+                System.out.println("You win");
+            } else {
+                if (guess < number) {
+                    System.out.println("less, go again");
+                } else {
+                    System.out.println("greater, go again");
+                }
+            }
+        }
+    }}
