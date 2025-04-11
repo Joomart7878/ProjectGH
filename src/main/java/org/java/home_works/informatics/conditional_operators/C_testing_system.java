745c69978284f0432375b29bbe0157fc723033f3
Joomart Abdykulov <joomart.abd@gmail.com>
2025-04-11 23:04:28 +0600

    test systems implem

--- src/main/java/org/java/home_works/informatics/conditional_operators/C_testing_system.java
+++ src/main/java/org/java/home_works/informatics/conditional_operators/C_testing_system.java
@@ -6,6 +6,15 @@ public class C_testing_system {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if (a == 1 && b == 1) {
+            System.out.println("YES");
+        } else if (a != 1 && b != 1) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
 
     }
 }
