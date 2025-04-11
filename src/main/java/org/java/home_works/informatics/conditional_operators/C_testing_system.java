b95949cc8ea603d50a4090429bfe9cd4be9ff3d2
Joomart Abdykulov <joomart.abd@gmail.com>
2025-04-11 22:53:15 +0600

    test systems

---
+++ src/main/java/org/java/home_works/informatics/conditional_operators/C_testing_system.java
@@ -0,0 +1,11 @@
+package org.java.home_works.informatics.conditional_operators;
+
+import java.util.Scanner;
+
+public class C_testing_system {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+    }
+}
