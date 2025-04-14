2e9147df546ccbf1410d0eb6ba225d82ca0d3817
Joomart Abdykulov <joomart.abd@gmail.com>
2025-04-14 22:09:13 +0600

    add implementation to the rook chess check

--- src/main/java/org/java/home_works/informatics/conditional_operators/G_rook_chess.java
+++ src/main/java/org/java/home_works/informatics/conditional_operators/G_rook_chess.java
@@ -1,4 +1,19 @@
 package org.java.home_works.informatics.conditional_operators;
 
+import java.util.Scanner;
+
 public class G_rook_chess {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rookRow = sc.nextInt();
+        int rookCol = sc.nextInt();
+        int pieceRow = sc.nextInt();
+        int pieceCol = sc.nextInt();
+
+        if (rookCol == pieceCol || rookRow == pieceRow) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
 }
