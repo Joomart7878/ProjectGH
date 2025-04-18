e06ead4f2a1183b0c7336f0ac2c64100357d6c6f
Joomart Abdykulov <joomart.abd@gmail.com>
2025-04-19 00:19:21 +0600

    add dog to the OOP

--- src/main/java/org/java/week_4/Dog.java
+++ src/main/java/org/java/week_4/Dog.java
@@ -1,9 +1,16 @@
 package org.java.week_4;
 
 public class Dog {
+//    public
+//    private
+//    package-private (default)
+//    protected
     public String name;
 
+    public Dog(String name) {
+        this.name = name;
+    }
     public void bark(){
-        System.out.println(this);
+        System.out.println(this.name + " barked");
     }
 }
---
+++ src/main/java/org/java/week_4/Main.java
@@ -0,0 +1,14 @@
+package org.java.week_4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog bobby = new Dog("Bobby");
+//        bobby.name = "Bobby";
+        bobby.bark();
+
+        Dog buddy = new Dog("Buddy");
+//        buddy.name = "Buddy";
+        buddy.bark();
+    }
+}
