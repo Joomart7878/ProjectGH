62b4a041c4f5758bf8ab1dd69d82c196eb9a4ad7
Joomart Abdykulov <joomart.abd@gmail.com>
2025-04-23 00:32:04 +0600

    if statement

---
+++ src/main/java/Cyber/If_statement.java
@@ -0,0 +1,31 @@
+package Cyber;
+
+public class If_statement {
+    public static void main(String[] args) {
+        if (9 > 2) {
+            System.out.println("Control operator");
+        }
+
+        int apples = 10;
+        int oranges = 5;
+
+        if (apples > oranges) {
+            System.out.println("I have more apples than oranges");
+        }
+
+        String userName = "abc";
+        String inputUserName = "abc";
+
+        if (userName.equals(inputUserName)) {
+            System.out.println("Access granted");
+            System.out.println("Welcome agent!");
+        }
+
+        int books = 50;
+
+        if (books < 100) {
+            books += 5;
+            System.out.println(books);
+        }
+    }
+}
