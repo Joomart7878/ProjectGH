000e18b38fd0228afb0535cff10efd38d2927ac2
Joomart Abdykulov <joomart.abd@gmail.com>
2025-04-24 23:50:24 +0600

    more multi branch

--- src/main/java/Cyber/Multi_branch_If_else.java
+++ src/main/java/Cyber/Multi_branch_If_else.java
@@ -1,5 +1,7 @@
 package Cyber;
 
+import java.util.Scanner;
+
 public class Multi_branch_If_else {
     public static void main(String[] args) {
         char grade = 'D';
@@ -12,6 +14,18 @@ public class Multi_branch_If_else {
             System.out.println("Not Bad");
         } else {
             System.out.println("Could be Better");
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your shoe size");
+        int size = sc.nextInt();
+
+        if(size > 12) {
+            System.out.println("Sorry, is not in stock");
+        } else if (size >= 6) {
+            System.out.println("Is in stock");
+        } else {
+            System.out.println("Soorry, this store only for big size");
         }
     }
 }
