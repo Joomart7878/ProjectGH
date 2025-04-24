7ac273f4081cdf58f7452225f79b631f9fdac415
Joomart Abdykulov <joomart.abd@gmail.com>
2025-04-24 23:45:57 +0600

    multi if ELSE

---
+++ src/main/java/Cyber/Multi_branch_If_else.java
@@ -0,0 +1,17 @@
+package Cyber;
+
+public class Multi_branch_If_else {
+    public static void main(String[] args) {
+        char grade = 'D';
+
+        if (grade == 'A') {
+            System.out.println("Excellent");
+        } else if (grade == 'B') {
+            System.out.println("Good");
+        } else if (grade == 'C') {
+            System.out.println("Not Bad");
+        } else {
+            System.out.println("Could be Better");
+        }
+    }
+}
