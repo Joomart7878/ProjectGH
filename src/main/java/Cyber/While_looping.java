e74cd369119435e0e14f55de72893a67f55e3495
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-04 20:49:22 +0600

    while loop statement

---
+++ src/main/java/Cyber/While_looping.java
@@ -0,0 +1,13 @@
+package Cyber;
+
+public class While_looping {
+    public static void main (String [] args) {
+        int apples = 1;
+
+        while (apples <= 10) {
+            System.out.println("Eating an apples");
+            apples++;
+        }
+        System.out.println("No more apples :(");
+    }
+}
