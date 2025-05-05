c4ff6b7ae122246770f4010a805b9ecdd53bff43
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-05 22:08:26 +0600

    for loop reversed

---
+++ src/main/java/Cyber/For_loop_a.java
@@ -0,0 +1,9 @@
+package Cyber;
+
+public class For_loop_a {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
