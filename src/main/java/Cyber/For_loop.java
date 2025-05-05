019aba1c481000aba07ae04c77fb075972fe43ee
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-05 22:08:06 +0600

    for loop

---
+++ src/main/java/Cyber/For_loop.java
@@ -0,0 +1,9 @@
+package Cyber;
+
+public class For_loop {
+    public static void main(String[] args) {
+        for (int n = 1; n <= 5; n++) {
+            System.out.println("Get me the number - " + n);
+        }
+    }
+}
