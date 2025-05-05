a38acad6fbca9623e15102f0d9ee9ff7de31edf6
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-05 10:45:25 +0600

    added the total number

--- src/main/java/Cyber/Do_while_loop.java
+++ src/main/java/Cyber/Do_while_loop.java
@@ -4,11 +4,12 @@ public class Do_while_loop {
     public static void main (String [] args) {
 
         int apples = 1;
+        int totalApples = 10;
 
         do{
-            System.out.println("Eating an apple");
-            apples++;
-        } while (apples <= 3);
+            System.out.println("Eating an apple - " + apples);
+            apples+=2;
+        } while (apples <= totalApples);
 
         System.out.println("No more apples");
     }
