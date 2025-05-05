bff05c593aeed7e89c04ebe3788ee5d0e29af887
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-05 10:42:03 +0600

    do while loop

---
+++ src/main/java/Cyber/Do_while_loop.java
@@ -0,0 +1,15 @@
+package Cyber;
+
+public class Do_while_loop {
+    public static void main (String [] args) {
+
+        int apples = 1;
+
+        do{
+            System.out.println("Eating an apple");
+            apples++;
+        } while (apples <= 3);
+
+        System.out.println("No more apples");
+    }
+}
