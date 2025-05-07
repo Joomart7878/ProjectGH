b5d38fc1c0baa9935f2faed56e8320caaa73fffa
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-07 21:20:48 +0600

    fruits arrays

---
+++ src/main/java/Cyber/For_loops_fruits.java
@@ -0,0 +1,14 @@
+package Cyber;
+
+public class For_loops_fruits {
+    public static void main(String[] args) {
+        String [] fruits = new String[3];
+        fruits [0] = "apples";
+        fruits [1] = "oranges";
+        fruits [2] = "pears";
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(fruits [i]);
+        }
+    }
+}
