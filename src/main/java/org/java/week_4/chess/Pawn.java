f4475064d3d613b848d2185a019937a75a56b4ef
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-12 13:47:20 +0600

    add pawn and knight moves form the chess

---
+++ src/main/java/org/java/week_4/chess/Pawn.java
@@ -0,0 +1,53 @@
+package org.java.week_4.chess;
+
+public class Pawn extends Piece {
+
+
+    Pawn (int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "x = " + this.x + "; " + "y = " + this.y;
+    }
+
+
+
+    // color is white
+    public boolean canMove(int nx, int ny) {
+        if (nx != x) {
+            return false;
+        }
+        if (x == 2) {
+            return  (y + 2 <= ny && ny > y);
+        } else {
+            return  (y + 1 == ny);
+        }
+    }
+
+    public void move(int nx, int ny) {
+        if (canMove(nx, ny)) {
+            this.x = nx;
+            this.y = ny;
+        }
+    }
+
+    public boolean canBeat(int nx, int ny) {
+        if (x + 1 != nx) {
+            return false;
+        }
+        return  (y + 1 == ny || y -1 == ny);
+    }
+
+    public void beat(int nx, int ny) {
+        if (canBeat(nx, ny)) {
+            this.x = nx;
+            this.y = ny;
+        }
+    }
+}
