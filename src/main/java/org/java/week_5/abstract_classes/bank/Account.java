41380598d0f10b40f41160970b20c39cdd12b920
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-12 18:59:43 +0600

    abstracted account classes

---
+++ src/main/java/org/java/week_5/abstract_classes/bank/Account.java
@@ -0,0 +1,17 @@
+package org.java.week_5.abstract_classes.bank;
+
+public abstract class Account {
+
+    protected int amount;
+
+    Account(int amount) {
+        this.amount = amount;
+    }
+
+    abstract void withdraw(int amount);
+    abstract void deposit(int amount);
+
+    int getCurrentAmount() {
+        return amount;
+    }
+}
---
+++ src/main/java/org/java/week_5/abstract_classes/bank/CryptoAccount.java
@@ -0,0 +1,11 @@
+package org.java.week_5.abstract_classes.bank;
+
+public class CryptoAccount {
+    void withdraw(int amount) {
+
+    }
+
+    void deposit(int amount) {
+
+    }
+}
