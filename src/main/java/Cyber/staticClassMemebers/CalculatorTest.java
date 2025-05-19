0517a6ab106b1f5dc2371cf8e3f29ac6ea28fbf9
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-19 12:37:38 +0600

    static method

---
+++ src/main/java/Cyber/staticClassMemebers/CalculatorTest.java
@@ -0,0 +1,13 @@
+package Cyber.staticClassMemebers;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(Calculator.plus(2, 5));
+        System.out.println(Calculator.minus(2, 5));
+        System.out.println(Calculator.multiply(2, 5));
+
+    }
+
+}
