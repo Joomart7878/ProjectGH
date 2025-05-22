9cf811d09b24bd16c22efbffa486deb75eba5d4b
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-23 00:48:42 +0600

    poly with an array

---
+++ src/main/java/Cyber/polymorphism/ShapesAray.java
@@ -0,0 +1,15 @@
+package Cyber.polymorphism;
+
+public class ShapesAray {
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[3];
+
+        shapes[0] = new Square();
+        shapes[1] = new Triangle();
+        shapes[2] = new Circle();
+
+        for (Shape sh : shapes) {
+            sh.draw();
+        }
+    }
+}
