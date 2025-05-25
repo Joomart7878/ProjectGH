0e8ae2a96495b2351b006519da34fbcc94ca64f1
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-25 20:50:03 +0600

    readfile class

---
+++ src/main/java/org/java/week_7/ReadFileExample.java
@@ -0,0 +1,16 @@
+package org.java.week_7;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class ReadFileExample {
+    void readFile(String fileName) {
+        FileReader in = new FileReader(fileName);
+
+        BufferedReader br = new BufferedReader(in);
+
+        try {
+            br.
+        }
+    }
+}
