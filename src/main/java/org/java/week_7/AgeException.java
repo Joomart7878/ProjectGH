aec5039e769a169b2c1914f98402be4fca766960
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-25 20:49:51 +0600

    add ageException

---
+++ src/main/java/org/java/week_7/AgeException.java
@@ -0,0 +1,9 @@
+package org.java.week_7;
+
+public class AgeException extends Exceptions{
+    public int age;
+    public AgeException(int age) {
+        super("User must be at least 18 to continue; the given user was " + age);
+        this.age = age;
+    }
+}
