7e9340a3cc54096ece1d7924c6335e222f9ca760
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-26 21:02:06 +0600

    add main class

---
+++ src/main/java/org/java/week_7/Main.java
@@ -0,0 +1,16 @@
+package org.java.week_7;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exceptions.example();
+        try {
+            ReadFileExample.readFile("src/week_7/input.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+    }
+
+}
