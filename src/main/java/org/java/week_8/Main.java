89661e5ad02ab32dffcbfd7a6d81d5dd772c6da6
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-29 22:12:19 +0600

    change the main class

--- src/main/java/org/java/week_8/Main.java
+++ src/main/java/org/java/week_8/Main.java
@@ -1,10 +1,21 @@
 package org.java.week_8;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        Input.example();
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Files.readAllLines(Path.of("src", "main", "java", "org", "java", "week_8", "input.txt"));
+        Map<String, Integer> count = new HashMap<>();
+        for (String line : lines) {
+            count.put(line, count.getOrDefault(line, 0) + 1);
+        }
+        for (String fruit : count.keySet()) {
+            System.out.printf("There are %d occurrences of %s\n", count.get(fruit), fruit);
+        }
     }
-
 }
