e2fcf03f6d67e31aec3c6182d0d43ea0ed0ef825
Joomart Abdykulov <joomart.abd@gmail.com>
2025-05-29 22:11:45 +0600

    add an output file exception reader

--- src/main/java/org/java/week_8/Input.java
+++ src/main/java/org/java/week_8/Input.java
@@ -1,23 +1,40 @@
 package org.java.week_8;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Input {
-    public static void main(String[] args) throws IOException {
-        InputStream is = new FileInputStream("src/main/java/org/java/week_8/input.txt");
-        byte[] allBytes = is.readAllBytes();
-        String result = new String(allBytes);
-        System.out.println(result);
+    public static void example() throws IOException {
+//        try-with-resources
+        try (InputStream is = new FileInputStream("src/main/java/org/java/week_8/input.txt")) {
+            List<Byte> bytesList = new ArrayList<>();
+            while (true) {
+                byte[] buffer = new byte[256];
 
-        Reader reader = new FileReader("src/main/java/org/java/week_8/input.txt");
-        char[] input = new char[256];
-        int n = reader.read(input);
-        System.out.println(new String(input, 0, n));
+                int n = is.read(buffer);
+                if (n == -1) {
+                    break;
+                }
+                for (byte b : buffer) {
+                    bytesList.add(b);
+                }
+            }
+            byte[] bytes = new byte[bytesList.size()];
+            for (int i = 0; i < bytes.length; ++i) {
+                bytes[i] = bytesList.get(i);
+            }
+            String result = new String(bytes);
+            System.out.println(result);
+        }
 
+        try (Reader reader = new FileReader("src/main/java/org/java/week_8/input.txt")) {
+            char[] input = new char[256];
 
+            int n = reader.read(input);
+            System.out.println(n);
 
-    }
-
-    public static void example() {
+            System.out.println(new String(input, 0, n));
+        }
     }
 }
