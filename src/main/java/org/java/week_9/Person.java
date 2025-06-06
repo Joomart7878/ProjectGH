215d8cb04b80b1b886624bbbf1659045aeceaed4
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-06 18:50:31 +0600

    add constuctors and set get to person

--- src/main/java/org/java/week_9/Person.java
+++ src/main/java/org/java/week_9/Person.java
@@ -2,7 +2,36 @@ package org.java.week_9;
 
 public class Person extends Human{
 
-    public Person(int age) {
+    private String name;
+    private String address;
+
+    public Person(int age, String name, String address) {
         super(age);
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
     }
 }
