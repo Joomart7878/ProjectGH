e1cb339a0e6abd95d0ee314a0080242436b573fe
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-06 19:25:30 +0600

    add the path to read the code

--- src/main/java/org/java/week_9/Main.java
+++ src/main/java/org/java/week_9/Main.java
@@ -1,13 +1,23 @@
 package org.java.week_9;
 
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Person joma = new Person(36, "Joomart", "Ulan2");
-        ObjectOutputStream oos = new ObjectOutputStream(System.out);
-        oos.writeObject(joma);
-        oos.flush();
+        try (FileOutputStream fos = new FileOutputStream("src/main/java/org/java/week_9/person.bin")){
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(joma);
+            oos.flush();
+        }
+        byte[] bytes = Files.readAllBytes(Path.of("src","main","java","org","java","week_9","person.bin"));
+        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+            Object o = ois.readObject();
+            Person p = (Person) o;
+            System.out.println(p);
+        }
+
     }
 }
