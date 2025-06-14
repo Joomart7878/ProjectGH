18fb030d672fe6e9bfa6c848007c262f4e06fe2c
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-15 00:26:45 +0600

    convert 1d to 2d leetcode

---
+++ src/main/java/Discret_Math/home_works/Java/solutions/Convert_1D_Into_2D.java
@@ -0,0 +1,18 @@
+package Discret_Math.home_works.Java.solutions;
+
+public class Convert_1D_Into_2D {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != n * n) {
+            return new int[0][0];
+        }
+        int[][] arr = new int[n][n];
+        int current_index = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = original[current_index];
+                current_index++;
+            }
+        }
+        return arr;
+    }
+}
