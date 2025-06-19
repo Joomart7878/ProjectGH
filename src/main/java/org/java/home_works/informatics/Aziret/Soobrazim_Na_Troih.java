edb8e9a78eab0cd8a3625fdd447d158a20cce19b
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-20 00:49:19 +0600

    soobrazim na troih implemetation

--- src/main/java/org/java/home_works/informatics/Aziret/Soobrazim_Na_Troih.java
+++ src/main/java/org/java/home_works/informatics/Aziret/Soobrazim_Na_Troih.java
@@ -3,5 +3,18 @@ package org.java.home_works.informatics.Aziret;
 import java.util.Scanner;
 
 public class Soobrazim_Na_Troih {
+    public static void main(String[] args)  throws java.lang.Exception{
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.next();
+        int sum = 0;
 
+        for (int i = 0; i < number.length(); i++) {
+            int digit = number.charAt(i) - '0';
+            sum += digit;
+        }
+        System.out.println(sum % 3 == 0 ? "YES" : "NO");
+
+
+
+    }
 }
