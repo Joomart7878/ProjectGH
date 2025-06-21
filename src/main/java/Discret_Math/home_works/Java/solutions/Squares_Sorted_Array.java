a328db95f5fc318afa592ed86456bf207408fd5f
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-21 14:10:04 +0600

    squares sorted array

---
+++ src/main/java/Discret_Math/home_works/Java/solutions/Squares_Sorted_Array.java
@@ -0,0 +1,14 @@
+package Discret_Math.home_works.Java.solutions;
+
+public class Squares_Sorted_Array {
+    public int[] sortedSquares(int[] A) {
+        int n = A.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+
+
+        return result;
+    }
+}
