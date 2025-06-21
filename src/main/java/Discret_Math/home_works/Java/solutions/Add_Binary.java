a801bd3d82b9d7daa29f45e0638afdbb102dbcda
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-21 14:09:21 +0600

    add while carry for binary

--- src/main/java/Discret_Math/home_works/Java/solutions/Add_Binary.java
+++ src/main/java/Discret_Math/home_works/Java/solutions/Add_Binary.java
@@ -6,7 +6,13 @@ public class Add_Binary {
         int i = a.length() - 1;
         int j = b.length() - 1;
         int carry = 0;
-
+        while (carry >= 0 || i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
+        }
         if (carry != 0) res.append(carry);
         return res.reverse().toString();
     }
