558cb614c4eeda691b855d7c809472b1a123f10a
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-22 14:11:34 +0600

    add a variable for sort colors

--- src/main/java/Discret_Math/home_works/Java/solutions/Sort_Colors.java
+++ src/main/java/Discret_Math/home_works/Java/solutions/Sort_Colors.java
@@ -2,7 +2,7 @@ package Discret_Math.home_works.Java.solutions;
 
 public class Sort_Colors {
     public void sortColors(int[] nums) {
-
+        int pos = 0;
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 1) swap(nums, i, pos++);
