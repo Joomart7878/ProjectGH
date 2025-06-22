7701640ef72903312820ec2fb53c7fd68fc36b39
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-22 14:11:10 +0600

    sort colors

---
+++ src/main/java/Discret_Math/home_works/Java/solutions/Sort_Colors.java
@@ -0,0 +1,18 @@
+package Discret_Math.home_works.Java.solutions;
+
+public class Sort_Colors {
+    public void sortColors(int[] nums) {
+
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 1) swap(nums, i, pos++);
+        }
+        for (int i = pos; i < nums.length; i++) {
+            if (nums[i] == 1) swap(nums, i, pos++);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j){
+
+    }
+}
