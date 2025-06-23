157f2a0c8caf7985373d379d11155d90eb5b5468
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-23 16:12:58 +0600

    backspace string

---
+++ src/main/java/Discret_Math/home_works/Java/solutions/Backspace_String_Copare.java
@@ -0,0 +1,16 @@
+package Discret_Math.home_works.Java.solutions;
+
+public class Backspace_String_Copare {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    private String build(String str){
+        StringBuilder builtStr = new StringBuilder();
+        for (char c : str.toCharArray()) {
+
+            }
+        }
+        return builtStr.toString();
+    }
+}
