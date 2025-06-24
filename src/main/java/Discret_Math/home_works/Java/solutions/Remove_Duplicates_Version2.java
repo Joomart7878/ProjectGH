574fbe576a817bad27b3c6a0c071a05d75eed27e
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-24 16:16:20 +0600

    run through for loop

--- src/main/java/Discret_Math/home_works/Java/solutions/Remove_Duplicates_Version2.java
+++ src/main/java/Discret_Math/home_works/Java/solutions/Remove_Duplicates_Version2.java
@@ -6,7 +6,14 @@ public class Remove_Duplicates_Version2 {
         int num_unique_elements = nums.length;
         nums_copy[0] = nums[0];
         int pointer = 1;
-
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                num_unique_elements--;
+            } else {
+                nums_copy[pointer] = nums[i];
+                pointer++;
+            }
+        }
         for (int i = 0; i < pointer; i++) {
             nums[i] = nums_copy[i];
         }
