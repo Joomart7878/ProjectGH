b9c4e34a7c61ad57a902d5021b16db334d48bc08
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-26 16:18:18 +0600

    add implementation to summary ranges

--- src/main/java/Discret_Math/home_works/Java/solutions/Summary_Ranges.java
+++ src/main/java/Discret_Math/home_works/Java/solutions/Summary_Ranges.java
@@ -4,5 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summary_Ranges {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> answer = new ArrayList<>();
 
+        int range_start = nums[0];
+        int range_end = -1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1] + 1) {
+                if (range_start == range_end) {
+                    answer.add(Integer.toString(range_start));
+                } else {
+                    answer.add(Integer.toString(range_start) + "->" + Integer.toString(range_end));
+                }
+                range_start = nums[i];
+            }
+        }
+        range_end = nums[nums.length - 1];
+        if (range_start == range_end) {
+            answer.add(Integer.toString(range_start));
+        } else {
+            answer.add(Integer.toString(range_start) + "->" + Integer.toString(range_end));
+        }
+        return answer;
+    }
 }
