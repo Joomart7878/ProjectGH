f56e2fd0ad5d18e9d5dd5b9694a58eb6e3abc53b
Joomart Abdykulov <joomart.abd@gmail.com>
2025-06-28 16:22:21 +0600

    chck valid parenthesees

---
+++ src/main/java/Discret_Math/home_works/Java/solutions/Valid_Parentheses.java
@@ -0,0 +1,16 @@
+package Discret_Math.home_works.Java.solutions;
+
+import java.util.Stack;
+
+public class Valid_Parentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') stack.push(')');
+            else if (c == '{') stack.push('}');
+            else if (c == '[') stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c) return false;
+        }
+
+    }
+}
