7a9b306e2d3323992db05d2af3118f7c17a67023
Joomart Abdykulov <joomart.abd@gmail.com>
2025-07-02 16:30:04 +0600

    start postion from 2

--- src/main/java/Discret_Math/home_works/Java/solutions/Remove_Duplicates_from_SortedArray.java
+++ src/main/java/Discret_Math/home_works/Java/solutions/Remove_Duplicates_from_SortedArray.java
@@ -2,7 +2,7 @@ package Discret_Math.home_works.Java.solutions;
 
 public class Remove_Duplicates_from_SortedArray {
     public int removeDuplicates(int[] nums) {
-        int insertPosition = 0;
+        int insertPosition = 2;
 
         for (int i = 2; i < nums.length; i++) {
             if (nums[i] != nums[insertPosition - 2]) {
