4262f3682784cce53ebf85cc9ac02c4b5905796d
Joomart Abdykulov <joomart.abd@gmail.com>
2025-07-03 16:31:00 +0600

    partition array

---
+++ src/main/java/Discret_Math/home_works/Java/solutions/Partition_Array.java
@@ -0,0 +1,22 @@
+package Discret_Math.home_works.Java.solutions;
+
+public class Partition_Array {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+
+        int[] result = new int[n];
+        int insertPosition = 0;
+
+        int pivotFreq = 0;
+
+        for (int num : nums) {
+            if (num < pivot) {
+                result[insertPosition] = num;
+                insertPosition++;
+            } else if (num == pivot) {
+                pivotFreq++;
+            }
+        }
+
+    }
+}
