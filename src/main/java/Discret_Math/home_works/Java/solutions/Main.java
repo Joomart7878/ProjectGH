5eef61b5e11ad286c494f36c644f08e58ec27b9a
Joomart Abdykulov <joomart.abd@gmail.com>
2025-08-20 00:58:43 +0600

    add more values

--- src/main/java/Discret_Math/home_works/Java/solutions/Main.java
+++ src/main/java/Discret_Math/home_works/Java/solutions/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Buy_And_Sell_Stocks stock = new Buy_And_Sell_Stocks();
 
-        int[] values = {66, 44};
+        int[] values = {66, 44, 33, 221, 343, 33, 32};
 
         int result = stock.maxProfit(values);
 
